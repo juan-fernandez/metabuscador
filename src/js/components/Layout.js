0078bd0b39c8f70357ce75fbb4569bdd48191cff
Juan <fernandez.alba.juan@gmail.com>
2017-03-13 08:55:37 +0100

    First commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { connect } from "react-redux"
+
+
+@connect((store) => {
+  return {
+
+  };
+})
+export default class Layout extends React.Component {
+
+  render() {
+
+    return
+    <div>
+      <h1>User Area<h1>
+    </div>
+  }
+}
---
+++ src/js/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux"
+
+import spaces from "./spacesReducer"
+
+export default combineReducers({
+    spaces,
+})
