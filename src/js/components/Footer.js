9b24d6e78ddde32fa36fa06033a364e4ddfc93b9
juan-fernandez <fernandez.alba.juan@gmail.com>
2017-03-19 16:29:06 +0100

    footer change with logo

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,7 +3,7 @@ import Paper from 'material-ui/Paper';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class Footer extends React.Component {
-    render(){
+   render(){
       const style={
          backgroundColor: '#3F51B5',
          color: '#fff',
@@ -12,17 +12,17 @@ export default class Footer extends React.Component {
          marginTop: '10px',
          marginBottom: '10px'
       }
-        return(
-           <MuiThemeProvider>
-           <Paper style={style} zDepth={2}>
-            <footer  class="text-center">
-              <p>Made by 7inEvent <a style={{color:'white'}} href="http://www.7inevent.com">7inevent.com</a></p>
-            </footer>
-           </Paper>
-           </MuiThemeProvider>
+      return(
+         <MuiThemeProvider>
+            <Paper style={style} zDepth={2}>
+               <footer  class="text-center">
+                  <p><img width="60px" src="resources/7_logo.png"/>Made by 7inEvent <a style={{color:'white'}} href="http://www.7inevent.com">7inevent.com</a></p>
+               </footer>
+            </Paper>
+         </MuiThemeProvider>
 
 
-        )
-    }
+      )
+   }
 
 }
